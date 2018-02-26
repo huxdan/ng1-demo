31770ac8637b41a11e91a07d94b496299cdf9f64
HXD <nbsg_huxiaodan@126.com>
2018-02-26 17:34:12 +0800

    use ui-router

--- src/app.js
+++ src/app.js
@@ -1,14 +1,21 @@
-var myApp = angular.module('myApp',[
-    'ngRoute'
+var myApp = angular.module('myApp',['ui.router'
 ]);
 
-myApp.config(['$locationProvider', '$routeProvider',
-    function ($locationProvider, $routeProvider) {
-        $locationProvider.html5Mode(true);
-        $routeProvider
-        .when('/home',{
-            templateUrl:'home/dashboard/dashboard.html',
-            controller:'dashboardCtrl'
-        }).otherwise({ redirectTo: '/home/dashoboard' });
+myApp.config(['$locationProvider', '$stateProvider',
+    function ($locationProvider, $stateProvider) {
+        $locationProvider.hashPrefix('');
+        $stateProvider
+        .state('home',{
+            url:'/',
+            views:{
+                '':{
+                    templateUrl:'home/dashboard/dashboard.html',
+                    controller:'dashboardCtrl'
+                },
+                'status':{
+                    template:'status pages'
+                }
+            }
+        })
     }   
 ]);
