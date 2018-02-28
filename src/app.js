98883427f6fa231a75d54cb1cb6b6797561f9f00
HXD <nbsg_huxiaodan@126.com>
2018-02-28 17:01:20 +0800

    carousel

--- src/app.js
+++ src/app.js
@@ -2,16 +2,16 @@ var myApp = angular.module('myApp',['ui.router'
 ]);
 
 myApp.config(['$locationProvider', '$stateProvider',
-    function ($locationProvider, $stateProvider) {
-        $locationProvider.hashPrefix('');
-        $stateProvider
-        .state('home',{
-            url:'/home',
-            templateUrl:'/home/dashboard/dashboard.html'
-        })
-        .state('about',{
-            url:'/about',
-            template:'THIS IS ABOUT PAGE'
-        })
-    }   
+  function ($locationProvider, $stateProvider) {
+    $locationProvider.hashPrefix('');
+    $stateProvider
+    .state('home',{
+      url:'/home',
+      templateUrl:'/home/dashboard/dashboard.html'
+    })
+    .state('about',{
+      url:'/about',
+      template:'THIS IS ABOUT PAGE'
+    })
+  }   
 ]);
