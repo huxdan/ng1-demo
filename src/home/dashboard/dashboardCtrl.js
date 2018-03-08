189552ef689833b60db5d1e1868f066a26461f88
huxdan <shodam@163.com>
2018-03-08 21:05:07 +0800

    sliphover

--- src/home/dashboard/dashboardCtrl.js
+++ src/home/dashboard/dashboardCtrl.js
@@ -1,5 +1,8 @@
 angular.module('myApp').controller('dashboardCtrl',['$scope',
   function($scope){
+
+      $('#slip-container').sliphover();
+
     $scope.title='Hello Dashboard...'
     $scope.onClick = function(){
       alert("sdf");      
