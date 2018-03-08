6b780da5bb21572fa46d1f2df35bbd1c489a7b90
huxdan <shodam@163.com>
2018-03-08 23:02:20 +0800

    temp

--- src/home/dashboard/dashboardCtrl.js
+++ src/home/dashboard/dashboardCtrl.js
@@ -1,5 +1,5 @@
-angular.module('myApp').controller('dashboardCtrl',['$scope',
-  function($scope){
+angular.module('myApp').controller('dashboardCtrl',['$scope','$location',
+  function($scope, $location){
 
       $('#slip-container').sliphover();
 
@@ -7,5 +7,43 @@ angular.module('myApp').controller('dashboardCtrl',['$scope',
     $scope.onClick = function(){
       alert("sdf");      
     }
+
+    $scope.imgClick = function(){
+      $location.url('about');
+    }
+
+    $scope.items = [
+      {
+        id: 1,
+        src: 'http://placehold.it/200x200'
+      },{
+        id: 2,
+        src: 'http://placehold.it/200x200'
+      },{
+        id: 3,
+        src: 'http://placehold.it/200x200'
+      },{
+        id: 4,
+        src: 'http://placehold.it/200x200'
+      },{
+        id: 5,
+        src: 'http://placehold.it/200x200'
+      },{
+        id: 6,
+        src: 'http://placehold.it/200x200'
+      },{
+        id: 7,
+        src: 'http://placehold.it/200x200'
+      },{
+        id: 8,
+        src: 'http://placehold.it/200x200'
+      },{
+        id: 9,
+        src: 'http://placehold.it/200x200'
+      },{
+        id: 10,
+        src: 'http://placehold.it/200x200'
+      }
+    ]
   }
 ])
