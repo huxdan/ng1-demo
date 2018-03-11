9699f2fc1a3351e1c81b6502b92eb3086270f977
huxdan <shodam@163.com>
2018-03-11 13:46:32 +0800

    avatar

--- src/home/post-detail/post-detailCtrl.js
+++ src/home/post-detail/post-detailCtrl.js
@@ -1,14 +1,14 @@
 angular.module('myApp').controller('post-detailCtrl',['$scope','$stateParams','$http',
   function($scope,$stateParams,$http){
     var param = $stateParams.id;
-    $scope.height = ($(window).height()-150) + 'px';
+    $scope.height = ($(window).height()-130) + 'px';
 
     $http.get('../../static/json/posts.json')
     .then(function(resp){
 
       angular.forEach(resp.data,function(data, index, array){
         if(data.id == param){
-          $scope.items  = data.details;
+          $scope.items  = data;
           console.log($scope.items);
         }
       });
